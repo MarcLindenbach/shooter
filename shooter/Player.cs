65a8e1dc26ba0290318d750123a3c33d728dc825
marc.lindenbach <marc.lindenbach@gmail.com>
2018-09-28 15:52:01 -0600

    Refactored player to a sprite

--- shooter/Player.cs
+++ shooter/Player.cs
@@ -7,28 +7,21 @@ namespace shooter
     public class Player
     {
         public Animation PlayerAnimation;
-        public Vector2 Position;
+        public Sprite PlayerSprite;
         public bool Active;
         public int Health;
-        public int Width => PlayerAnimation.FrameWidth;
-        public int Height => PlayerAnimation.FrameHeight;
-
-        public int Left => (int)Position.X;
-        public int Right => (int)Position.X + Width;
-        public int Top => (int)Position.Y;
-        public int Bottom => (int)Position.Y + Height;
 
         public void Initialize(Animation animation, Vector2 position)
         {
             PlayerAnimation = animation;
-            Position = position;
+            PlayerSprite = new Sprite(position, animation.FrameWidth, animation.FrameHeight);
             Active = true;
             Health = 100;
         }
 
         public void Update(GameTime gameTime)
         {
-            PlayerAnimation.Position = Position;
+            PlayerAnimation.Position = PlayerSprite.Position;
             PlayerAnimation.Update(gameTime);
         }
 
